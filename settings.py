96995008ac1c5d81ecfc77a6068780b350e7754e
Grigorii <Grigorii@eduroam-int-dhcp-97-118-123.ucl.ac.uk>
2024-12-12 13:04:43 +0000

    Add CORS middleware5

--- settings.py
+++ settings.py
@@ -5,35 +5,29 @@ class CorsMiddleware:
         self.app = app
         
     def __call__(self, environ, start_response):
-        # Добавляем обработку всех методов, не только OPTIONS
+        # Добавляем слэш в конец URL если его нет
+        path_info = environ.get('PATH_INFO', '')
+        if path_info.startswith('/api/') and not path_info.endswith('/'):
+            environ['PATH_INFO'] = path_info + '/'
+        
         def custom_start_response(status, headers, exc_info=None):
             headers = list(headers)
-            # Всегда добавляем CORS заголовки
-            headers.append(('Access-Control-Allow-Origin', 'https://gregory-ch.github.io'))
-            headers.append(('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'))
-            headers.append(('Access-Control-Allow-Headers', 'Content-Type, otree-rest-key'))
-            headers.append(('Access-Control-Allow-Credentials', 'true'))
-            headers.append(('Access-Control-Max-Age', '86400'))
-            
-            # Если это перенаправление (300-308), добавляем CORS заголовки
-            if status.startswith(('301', '302', '303', '307', '308')):
-                for i, (header, value) in enumerate(headers):
-                    if header == 'Location':
-                        # Сохраняем CORS заголовки при перенаправлении
-                        headers[i] = ('Access-Control-Expose-Headers', 'Location')
-                        break
-            
+            # Добавляем CORS заголовки независимо от статуса ответа
+            cors_headers = [
+                ('Access-Control-Allow-Origin', 'https://gregory-ch.github.io'),
+                ('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'),
+                ('Access-Control-Allow-Headers', 'Content-Type, otree-rest-key'),
+            ]
+            headers.extend(cors_headers)
             return start_response(status, headers, exc_info)
-        
+            
         if environ.get('REQUEST_METHOD') == 'OPTIONS':
             headers = [
                 ('Access-Control-Allow-Origin', 'https://gregory-ch.github.io'),
                 ('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'),
                 ('Access-Control-Allow-Headers', 'Content-Type, otree-rest-key'),
-                ('Access-Control-Allow-Credentials', 'true'),
-                ('Access-Control-Max-Age', '86400'),
                 ('Content-Type', 'text/plain'),
-                ('Content-Length', '0'),
+                ('Content-Length', '0')
             ]
             start_response('200 OK', headers)
             return [b'']
@@ -196,224 +190,5 @@ PARTICIPANT_FIELDS = [
 SESSION_FIELDS = ['finished_p1_list', 'iowa_costs', 'wisconsin', 'intergenerational_history']
 
 MIDDLEWARE = [
-    'settings.CorsMiddleware',  # Должен быть первым!
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'settings.CorsMiddleware',  # Должен быть первым
 ]
-
-# CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "https://gregory-ch.github.io",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
-
-CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOW_METHODS = [
-    'GET',
-    'POST',
-    'OPTIONS',
-]
-
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
-
-# For iframe embedding
-X_FRAME_OPTIONS = 'ALLOW-FROM https://gregory-ch.github.io'
-CSRF_TRUSTED_ORIGINS = ['https://gregory-ch.github.io']
-
-# Добавьте эти настройки
-CORS_ALLOW_CREDENTIALS = True
-CORS_REPLACE_HTTPS_REFERER = True
-CORS_URLS_REGEX = r'^/api/.*$'
-
-
-# dict(
-    #     name='intergenerational',
-    #     app_sequence=['intergenerational'],
-    #     display_name="""
-    #     Intergenerational/evolutionary game, passing along donations to future players, 
-    #     in a chain.""",
-    #     num_demo_participants=6,
-    # ),
-    # dict(
-    #     name='scheduling',
-    #     display_name="Scheduling players to arrive at the same time",
-    #     num_demo_participants=12,
-    #     app_sequence=['scheduling_part0', 'scheduling_part1'],
-    # ),
-    # dict(
-    #     name='supergames_indefinite',
-    #     display_name="Supergames of an indefinitely repeated prisoner's dilemma",
-    #     num_demo_participants=2,
-    #     app_sequence=['supergames_indefinite'],
-    # ),
-    # dict(
-    #     name='wait_page_from_scratch',
-    #     display_name="Wait page implemented from scratch, using live pages.",
-    #     num_demo_participants=6,
-    #     app_sequence=['wait_page_from_scratch'],
-    # ),
-    # dict(
-    #     name='fast_consensus',
-    #     display_name="Fast consensus: Reach a consensus with your group before your payoffs shrink to 0.",
-    #     num_demo_participants=3,
-    #     app_sequence=['fast_consensus'],
-    # ),
-    # dict(
-    #     name='bots_vs_humans',
-    #     display_name="Bots playing against a human participant",
-    #     num_demo_participants=1,
-    #     app_sequence=['bots_vs_humans'],
-    # ),
-    # dict(
-    #     name='crazy_eights',
-    #     display_name="Card game (crazy eights)",
-    #     app_sequence=['crazy_eights'],
-    #     num_demo_participants=2,
-    # ),
-    # dict(
-    #     name='word_search',
-    #     display_name="Word search game (multiplayer)",
-    #     app_sequence=['word_search'],
-    #     num_demo_participants=2,
-    # ),
-    # dict(
-    #     name='punishment',
-    #     display_name="Public goods with punishemnt",
-    #     app_sequence=['punishment'],
-    #     num_demo_participants=4,
-    # ),
-    # dict(
-    #     name='continuous_time_slider',
-    #     display_name="Continuous-time public goods game with slider",
-    #     app_sequence=['continuous_time_slider'],
-    #     num_demo_participants=2,
-    # ),
-    # dict(
-    #     name='asynchronous',
-    #     app_sequence=['asynchronous'],
-    #     display_name="Asynchronous 2-player game (no waiting for another player)",
-    #     num_demo_participants=6,
-    # ),
-    # dict(
-    #     name='wisconsin',
-    #     display_name="Wisconsin Card Sorting Test",
-    #     app_sequence=['wisconsin'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='iowa_gambling',
-    #     display_name="Iowa Gambling Task",
-    #     app_sequence=['iowa_gambling'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='twitter',
-    #     app_sequence=['twitter'],
-    #     display_name="Mini-Twitter",
-    #     num_demo_participants=6,
-    # ),
-    # dict(
-    #     name='svo',
-    #     display_name="Social Value Orientation Measure (SVO)",
-    #     app_sequence=['svo'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='image_annotation',
-    #     display_name="Image annotation",
-    #     app_sequence=['image_annotation'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='image_rating',
-    #     display_name="Image rating",
-    #     app_sequence=['image_rating'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='live_coordination',
-    #     display_name="Live coordination (voting with chat/negotiation)",
-    #     app_sequence=['live_coordination'],
-    #     num_demo_participants=6,
-    # ),
-    # dict(
-    #     name='shop',
-    #     display_name="Shopping app (online grocery store)",
-    #     app_sequence=['shop'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='double_auction',
-    #     display_name="Double auction market",
-    #     app_sequence=['double_auction'],
-    #     num_demo_participants=4,
-    # ),
-    # dict(name='dollar_auction', app_sequence=['dollar_auction'], num_demo_participants=3),
-    # dict(
-    #     name='ebay',
-    #     display_name="eBay style auction",
-    #     app_sequence=['ebay'],
-    #     num_demo_participants=3,
-    # ),
-    # dict(name='stroop', app_sequence=['stroop'], num_demo_participants=1),
-    # dict(
-    #     name='go_no_go',
-    #     display_name='Attention test (Go/No-Go)',
-    #     app_sequence=['go_no_go'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='choice_list',
-    #     display_name='Choice list (Holt/Laury, equivalence test, etc)',
-    #     app_sequence=['choice_list'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='live_bargaining',
-    #     display_name="Live bargaining between 2 players",
-    #     app_sequence=['live_bargaining'],
-    #     num_demo_participants=2,
-    # ),
-    # dict(
-    #     name='randomize_stimuli',
-    #     display_name='Demo of different stimulus randomizations',
-    #     app_sequence=['randomize_stimuli'],
-    #     num_demo_participants=5,
-    # ),
-    # dict(
-    #     name='bigfive',
-    #     display_name='Big 5 personality test',
-    #     app_sequence=['bigfive'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='panas',
-    #     display_name='PANAS (positive and negative affect schedule)',
-    #     app_sequence=['panas'],
-    #     num_demo_participants=1,
-    # ),
-    # dict(
-    #     name='nim',
-    #     display_name="Race game / Nim (take turns adding numbers to reach a target)",
-    #     app_sequence=['nim'],
-    #     num_demo_participants=2,
-    # ),
-    # dict(
-    #     name='read_mind_in_eyes',
-    #     display_name="Reading the Mind in the Eyes Test (Baron-Cohen et al. 2001)",
-    #     app_sequence=['read_mind_in_eyes'],
-    #     num_demo_participants=1,
