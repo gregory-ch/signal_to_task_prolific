fb9645c91fa09905ee315b4be0f0ad92a28e96b1
gregory-ch <46967700+gregory-ch@users.noreply.github.com>
2025-01-31 16:01:26 +0000

    prod prolific 1==1

--- settings.py
+++ settings.py
@@ -49,7 +49,7 @@ SESSION_CONFIGS = [
 # the session config can be accessed from methods in your apps as self.session.config,
 # e.g. self.session.config['participation_fee']
 
-SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=0.09, participation_fee=00.00, doc="")
+SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=00.00, doc="")
 
 # ISO-639 code
 # for example: de, fr, ja, ko,'ru', zh-hans
